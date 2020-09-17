0fe6b5b9ac1ff5639d5b41f73d2ca95941c9a7dd
kawamotoseiya <seinnto1226@icloud.com>
2020-09-17 20:08:04 +0900

    json

--- nokogiri_sample.rb
+++ nokogiri_sample.rb
@@ -3,5 +3,14 @@ require 'nokogiri'
 html = File.open('pitnews.html', 'r') {|f| f.read }
 doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
 
-nodes = doc.xpath('//h6')
-nodes.each { |node| pp node }
+section = doc.xpath('/html/body/main/section[2]').first
+contents = {category: nil, news: []}
+contents[:category] = section.xpath('./h6').first.text
+
+section.xpath('./div/div').each do |node|
+  title = node.xpath('./p/strong/a').first.text
+  url = node.xpath('./p/strong/a').first['href']
+  news = {title: title, url: url}
+  contents[:news] << news
+end
+pp contents
--- scrape.rb
+++ scrape.rb
@@ -8,6 +8,28 @@ def write_file(path, text)
   File.open(path, 'w') { |file| file.write(text) }
 end
 
-html = get_from('https://masayuki14.github.io/pit-news/')
+require 'nokogiri'
 
-write_file('pitnews.html', html)
+html = File.open('pitnews.html', 'r') {|f| f.read }
+doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
+
+pitnews = []
+doc.xpath('/html/body/main/section').each_with_index do |section, index|
+
+  next if index.zero?
+  contents = {category: nil, news: []}
+  contents[:category] = section.xpath('./h6').first.text
+
+  section.xpath('./div/div').each do |node|
+    title = node.xpath('./p/strong/a').first.text
+    url = node.xpath('./p/strong/a').first['href']
+
+    news = {title: title, url: url}
+    contents[:news] << news
+  end
+
+  pitnews << contents
+end
+require 'json'
+File.open('pitnews.json', 'w') { |file| file.write({pitnews: pitnews}.to_json) }
